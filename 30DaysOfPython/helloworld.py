7c1f5745e00e4288aba1a3a43d4de182c06b9b95
JosephSonntag <117291952+JosephSonntag@users.noreply.github.com>
2025-08-06 12:42:41 +0200

    q

---
+++ 30DaysOfPython/helloworld.py
@@ -0,0 +1,19 @@
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)        # addition
+print(2 - 3)        # subtraction
+print(2 * 3)        # multiplication
+print(2 / 3)        # division
+print(2 ** 3)       # exponential
+print(2 % 3)        # modulus
+print(2 // 3)       # Floor division operator
+
+# Checking data types
+print(type(10))     # int
+print(type(3.14))     # float
+print(type(1 + 3j))     # Complex Number
+print(type('Asabeneh'))     # string
+print(type([1, 2, 3]))     # list
+print(type({'name':'Asabaneh'}))     # dictionary
+print(type({9.8, 3.14, 2.7}))     # set
+print(type((9.8, 3.14, 2.7)))     # tuple
